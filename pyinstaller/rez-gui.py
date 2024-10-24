1ee62bb6eeb06bcb1afcf4b5970328833b2ddb3d
FhyTan <FhyTan@outlook.com>
2024-10-24 13:10:18 +0800

    add pyinstaller build scripts

---
+++ pyinstaller/rez-gui.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from rez.cli._main import run
+from rez.system import system
+
+# Determine if rez is running in a production install
+if os.path.basename(sys.executable) == "rez-gui.exe":
+    system.rez_bin_path = os.path.dirname(sys.executable)
+
+# Run the command
+run("gui")
